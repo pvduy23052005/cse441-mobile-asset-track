f4baae3ea094d2e4923b07e14b0a081a2f965e2c
Phùng Văn Duy <phungvanduy23052005@gmail.com>
2026-08-02 09:30:20 +0700

    feat: config module connect into supabase

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppService } from './app.service';
+import { SupabaseModule } from './modules/supabase/supabase.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    SupabaseModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
